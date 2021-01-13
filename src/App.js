598c2551c411455483be6db3e674e6c16d18c4b9
Florence <monnaie.florence@gmail.com>
2021-01-13 10:35:57 +0100

    Partie2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import './style.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Exo 1</h1>
+      <p className="text-green">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores, quaerat distinctio mollitia quasi et praesentium corporis ad hic deserunt magnam.</p>
     </div>
   );
 }
